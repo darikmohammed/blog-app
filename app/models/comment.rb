b9efdfa6b6142dfae63cc29ce3d5b8628ebe0d57
darikmohammed <r.m.darik@gmail.com>
2022-09-14 23:04:34 +0300

    Add comment counter updater

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,11 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
+
+  after_create :update_post_comment_counter
+  def update_post_comment_counter
+    post.comments_counter = 0 unless post.comments_counter?
+    post.comments_counter += 1
+    post.save
+  end
 end
