c0f3d715639449ecdfde45a4661d582c9ed04c72
darikmohammed <r.m.darik@gmail.com>
2022-09-20 23:43:05 +0300

    Add testing for Posts controller

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe 'Users', type: :request do
       expect(response).to have_http_status(:success)
     end
     it 'returns correct rendering' do
-      expect(response.body).to include('Users#index')
+      expect(response.body).to render_template(:index)
     end
     it 'returns correct body placeholder' do
       expect(response.body).to include('Find me in app/views/users/index.html.erb')
@@ -31,3 +31,4 @@ RSpec.describe 'Users', type: :request do
     end
   end
 end
+
