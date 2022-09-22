26e0d75b556abcc5cd3fe0d7136bfde58f6852d8
darikmohammed <r.m.darik@gmail.com>
2022-09-22 18:09:46 +0300

    Update Link in posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,11 @@
 class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    @posts= Post.includes(:comments, :author).where(author: @user)
+    @posts = Post.includes(:comments, :author).where(author: @user)
   end
 
   def show
-    @post = Post.find_by(id:params[:id], author_id: params[:user_id])
+    @post = Post.find_by(id: params[:id], author_id: params[:user_id])
     @comments = @post.most_recent_comments
   end
 end
