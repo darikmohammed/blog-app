dd9382b37ef9f5ef50af70ba0bb9e515376287f8
darikmohammed <r.m.darik@gmail.com>
2022-09-23 19:26:01 +0300

    Finish the function to add comment in post

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,13 @@ class CommentsController < ApplicationController
     @recent_comments = @post.most_recent_comments
   end
   def create
+    comment = Comment.new(author_id: params[:user_id], post_id: params[:post_id], text: params[:text])
+    comment.save
 
+    if comment.save
+      redirect_to user_post_path(user_id: params[:user_id], id: params[:post_id])
+    else
+      redirect_to new_user_post_comment_path
+    end
   end
 end
