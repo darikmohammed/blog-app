76f61af06aa16e12373a05f9d0f13001def8bf35
darikmohammed <r.m.darik@gmail.com>
2022-09-27 11:08:10 +0300

    Add testing for user index page

---
+++ spec/feature/user_index_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe 'User Index page:', type: :feature do
+  let!(:user) do
+    User.create(name: 'Maqueen', photo: 'https://media.istockphoto.com/vectors/default-profile-picture-avatar-photo-placeholder-vector-illustration-vector-id1223671392?k=20&m=1223671392&s=612x612&w=0&h=lGpj2vWAI3WUT1JeJWm1PRoHT3V15_1pdcTn2szdwQ0=',
+                bio: 'Programer in headquaters', posts_counter: 0)
+  end
+  it "should show the root directory" do
+    visit '/'
+    expect(current_path).to eq(root_path)
+  end
+
+  it 'I can see the username of all other users.' do
+    visit users_path
+    expect(page).to have_text(user.name)
+  end
+
+  it 'I can see the number of posts each user has written.' do
+    visit users_path
+    expect(page).to have_text(/#{user.posts_counter} posts/i)
+  end
+
+  it 'I can see the profile picture for each user' do
+    visit users_path
+    expect(page).to have_xpath("//img[@src ='#{user.photo}']")
+  end
+
+  it "When I click on a user, I am redirected to that user's show page' " do
+    visit users_path
+    click_on user.name
+    expect(page).to have_current_path(user_path(user))
+  end
+end
